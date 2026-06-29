9d2b714dd5f57ebfc9001009ee918898c6c1b3ff
Emil H. Clausen <sequoiia@hummel.yt>
2026-06-29 15:07:43 +0200

    chore(migrations): remove identity null index backfill migration

--- backend/migrations/2026-06-29-000000_backfill_identity_null_index/down.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX CONCURRENTLY IF EXISTS idx_ct_identity_source_null;
--- backend/migrations/2026-06-29-000000_backfill_identity_null_index/up.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_ct_identity_source_null
-    ON cloudtrail_events (event_time DESC)
-    WHERE identity_source IS NULL;
